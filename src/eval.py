e599131666116c5e9b03cc72f7310f13110c8e06
Nguyen Hai Dang <dangnguyen29124@gmail.com>
2025-03-28 19:05:53 +0700

    Refactor dataset.py

--- src/eval.py
+++ src/eval.py
@@ -5,21 +5,23 @@ import torch
 from torch.utils.data import DataLoader
 
 from src.model.unet import UNet3D
-from src.dataset import ACDC, ACDCProcessed, JustToTest
+from src.dataset.acdc import ACDC
+from src.dataset.just_to_test import JustToTest
 from src.metrics import calculate_multiclass_dice
+
 
 if __name__ == "__main__": 
     os.environ["CUDA_VISIBLE_DEVICES"] = "0"
     
     device = "cuda"
-    model_path = "model/unet3d_6.pth"
+    model_path = "model/unet3d_8.pth"
     print(f"Using {model_path}")
 
     model = UNet3D().to(device)
 
     model.load_state_dict(torch.load(model_path))
 
-    test_dataset = JustToTest("just_to_test/testing", is_testset=True)
+    test_dataset = ACDC("database/testing", is_testset=True)
     test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     dices = []
@@ -46,15 +48,15 @@ if __name__ == "__main__":
             class3_dice.append(dice_list[2])
             dices.append(dice)  
             
-            print(f"{dice_list} Dice: {dice:.3f}")
+            print(f"Dice: {dice:.3f} {np.round(dice_list, 3)} ")
                       
 
     mean_dice_class1 = np.mean(class1_dice)
     mean_dice_class2 = np.mean(class2_dice)
     mean_dice_class3 = np.mean(class3_dice)
 
-    print("Mean Dice of class 1: ", mean_dice_class1)
-    print("Mean Dice of class 2: ", mean_dice_class2)
-    print("Mean Dice of class 3: ", mean_dice_class3)
-    print("Mean Dice: ", np.mean(dices))
+    print(f"Mean Dice of class 1: {mean_dice_class1:.3f}") 
+    print(f"Mean Dice of class 2: {mean_dice_class2:.3f}", )
+    print(f"Mean Dice of class 3: {mean_dice_class3:.3f}", )
+    print(f"Mean Dice: ", np.round(np.mean(dices), 3))
 
